7112b0cc4f596ee3d49359580a25fc53e23df4db
Keith Compere <keithcompere150@gmail.com>
2022-11-08 15:20:30 -0500

    Starting heap attempt

---
+++ Heap_Attempt.java
@@ -0,0 +1,22 @@
+public class Heap_Attempt {
+    public static class Node{
+        private int val;
+        private Node parent;
+        private Node left;
+        private Node right;
+    }
+    private Node head;
+    
+    public Heap_Attempt(){
+        this.head = null;
+    }
+    public void push(Node node){
+        if(head == null){
+            head = node;
+        }
+        else{
+            
+        }
+    }
+    
+}
