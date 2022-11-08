36cf0a0e1350625138c3e6dfeaa7cb74a3ae8e5c
Keith Compere <keithcompere150@gmail.com>
2022-11-08 15:07:29 -0500

    Made Bubblesort

--- BubbleSort.java
+++ BubbleSort.java
@@ -1,19 +1,22 @@
-import java.sql.Array;
 
 public class BubbleSort {
 
     public static void main(String[] args){
-        
+        int[] array = new int[]{1,7,2,6,0};
+        BSort(array);
+        for(int i=0;i<array.length;i++){
+            System.out.println(array[i]);
+        }
     }
     public static void BSort(int[] array) {
         boolean flag =false;
         while(flag == false){
                 int swap =0;
                 for(int i=0;i<array.length;i++){
-                    if(i<array.length-2 && array[i]>array[i+1]){
+                    if(i<=array.length-2 && array[i]>array[i+1]){
                         int tmp = array[i];
-                        array[i+1]=array[i];
-                        array[i] = tmp;
+                        array[i]=array[i+1];
+                        array[i+1] = tmp;
                         swap++;
                     }
                 }
