57b9e7dbc56f567783bd90dd607eba5c71ef5ae2
Keith Compere <keithcompere150@gmail.com>
2022-11-08 15:26:31 -0500

    Making functions

--- Heap_Attempt.java
+++ Heap_Attempt.java
@@ -1,22 +1,38 @@
 public class Heap_Attempt {
+    //This heap is a min-heap since that is probably going to be more useful for when I use heap sort
     public static class Node{
         private int val;
         private Node parent;
         private Node left;
         private Node right;
+
+        public Boolean isExternal(){
+            if(left == null && right == null){
+                return(true);
+            }
+            return(false);
+        }
     }
     private Node head;
+    private Node next_external;
     
     public Heap_Attempt(){
         this.head = null;
     }
+
     public void push(Node node){
         if(head == null){
             head = node;
+            next_external = node;
         }
         else{
-            
+
         }
     }
+
+    public void heapify(Node node){
+        //upheap
+    }
+
     
 }
