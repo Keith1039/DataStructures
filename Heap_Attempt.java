bc8b166b35026133058cbd19f9e703b2d60715b4
Keith Compere <keithcompere150@gmail.com>
2022-11-09 09:25:51 -0500

    working on heap attempt

--- Heap_Attempt.java
+++ Heap_Attempt.java
@@ -1,3 +1,4 @@
+
 public class Heap_Attempt {
     //This heap is a min-heap since that is probably going to be more useful for when I use heap sort
     public static class Node{
@@ -5,33 +6,59 @@ public class Heap_Attempt {
         private Node parent;
         private Node left;
         private Node right;
+        private int depth;
+        private int pos;
 
         public Boolean isExternal(){
-            if(left == null && right == null){
-                return(true);
-            }
-            return(false);
+            return(left==null && right ==null);
+        }
+        public boolean isFull(){
+            return(left != null && right != null);
+        }
+        public boolean isMostRight(){
+            return(pos == (int)(Math.pow(2, depth+1)-1));
         }
     }
     private Node head;
     private Node next_external;
+    private int size =0;
     
     public Heap_Attempt(){
         this.head = null;
     }
 
     public void push(Node node){
+        size++;
+        node.pos = size;
         if(head == null){
             head = node;
             next_external = node;
+            head.depth =0;
         }
         else{
-
+            node.depth=node.parent.depth+1;
+            node.parent = next_external;
+            if(next_external.left == null){
+                next_external.left = node;
+            }
+            else{
+                next_external.right = node;
+            }
         }
+        
     }
 
     public void heapify(Node node){
-        //upheap
+        //upheap meant to be active only after data is pushed by the push function.
+        if(node.parent.val > node.val){
+            //swaps values 
+            int val = node.parent.val;
+            node.parent.val = node.val;
+            node.val = val;
+        }
+    }
+    public void findNextExternal(){
+        //Finds the next external node to populate.
     }
 
     
